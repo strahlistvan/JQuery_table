96860f48ef94d48f557006277f33fe2191b5d0c3
strahlistvan <strahlistvan@users.noreply.github.com>
2016-07-04 22:27:03 +0200

    First commit

---
+++ tablesorter.js
@@ -0,0 +1,114 @@
+$(function() {
+		var tableMatrix = [];
+		var sortKey = 0;
+		var callerID = "";  //store the caller element ID (ugly)
+		var reverse = false;
+
+	   /** Stringify parameter object, or parse it to number (if it's numeric)  */
+		function goodFormat(value)
+		{
+			if (isNaN(value))
+				return value.toString().trim();
+			if (value == parseInt(value))
+				return parseInt(value);
+			else
+				return parseFloat(value);
+		}
+		
+		/** Sort the tableMatrix variable. 
+		  * Sort key: index of the selected column
+		 */
+		var sortTableByKey = function(sortKey)
+		{
+			//Simple sorting algorithm:
+			var N = tableMatrix[sortKey].length;
+			for (var i=0 ; i<N; ++i)
+			{
+				for (var j=i; j<N; ++j)
+				{
+				    //two elements: if strings, lower case
+					var elem_i = (isNaN(tableMatrix[sortKey][i])) ? tableMatrix[sortKey][i].toString().toLowerCase().trim() : tableMatrix[sortKey][i];
+					var elem_j = (isNaN(tableMatrix[sortKey][j])) ? tableMatrix[sortKey][j].toString().toLowerCase().trim() : tableMatrix[sortKey][j];
+						
+					if ( (elem_i > elem_j && !reverse)
+						|| (elem_i < elem_j && reverse))
+					{			
+					    //swap the elements in all columns
+						for (var k in tableMatrix)
+						{
+							var tmp = tableMatrix[k][i];
+							tableMatrix[k][i] = tableMatrix[k][j];
+							tableMatrix[k][j] =tmp;
+						}
+					}
+				}
+			}
+			
+			$("#"+callerID).find("tbody tr").each(function() {
+				var rowIndex = $(this).index();
+				var row = this;
+							
+				$(row).find("td").each(function(){
+					var colIndex = $(this).index();
+					var data = tableMatrix[colIndex][rowIndex]
+					$(this).html(data); //insert data to HTML DOM
+				});
+							
+			});
+			reverse = !reverse;
+			console.log(JSON.stringify(tableMatrix));
+		}
+				
+		$.fn.sortable = function()
+		{
+		
+			//Currently the table must contain thead and tbody children
+			if (!$(this).find("thead tr th") || 
+			     !$(this).find("tbody tr td") )
+			{
+				alert("Error! $("+$(this).selector+") element is not a valid  HTML table");
+				return;
+			}
+		
+			//Currently the sortable selector must be unique
+			if ($(this).length > 1)
+			{
+				alert($(this).length);
+				alert("Error! $("+$(this).selector+") Table selector is not unique!");
+				return;
+			}
+		
+			//if not have id, we add one (ugly solution)
+			if (!$(this).attr("id"))
+			{
+				$(this).attr("id", (new Date()).getTime());
+			}
+			//Store the caller element's ID	
+			 callerID = $(this).attr("id");   
+			
+			$(this).find("thead tr th").each(function(){
+				var colIndex = $(this).index();
+				tableMatrix[colIndex] = [ ];
+							
+				//Table styling...
+				$(this).hover(function() {
+						$(this).css("cursor", "pointer");
+				});
+				
+				//click event on th element
+				$(this).on("click", function(event){
+					sortKey = $(event.target).index();
+					sortTableByKey(sortKey);		
+				});
+							
+			});
+										
+			//Put data from HTML table to tableMatrix variable
+			$(this).find("tbody tr td").each(function(){
+					var colIndex = $(this).index();
+					var data = goodFormat($(this).html());
+					console.log(typeof data);
+					tableMatrix[colIndex].push(data);	
+			});
+		}
+});
