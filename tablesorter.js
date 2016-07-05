258246a4104fad51875a6a00105ccc464bb4f6ca
strahlistvan <strahlistvan@users.noreply.github.com>
2016-07-05 14:22:39 +0200

    Now it works to multiple elements too

--- tablesorter.js
+++ tablesorter.js
@@ -1,7 +1,13 @@
-$(function() {
-		var tableMatrix = [];
+(function( $ ) {
+		
+		/** Sortable function */		
+		$.fn.sortable = function()
+		{
+			return this.each(function() {
+					var tableMatrix = [];
 		var sortKey = 0;
 		var callerID = "";  //store the caller element ID (ugly)
+		var activeTable;  //store the active table element
 		var reverse = false;
 
 	   /** Stringify parameter object, or parse it to number (if it's numeric)  */
@@ -43,8 +49,9 @@ $(function() {
 					}
 				}
 			}
-			
-			$("#"+callerID).find("tbody tr").each(function() {
+			//Insert sorted data to HTML table
+			console.log("act table id:: "+activeTable.attr("id"));
+			activeTable.find("tbody tr").each(function() {
 				var rowIndex = $(this).index();
 				var row = this;
 							
@@ -54,61 +61,65 @@ $(function() {
 					$(this).html(data); //insert data to HTML DOM
 				});
 							
-			});
+			}); 
 			reverse = !reverse;
 			console.log(JSON.stringify(tableMatrix));
 		}
 				
-		$.fn.sortable = function()
-		{
-		
-			//Currently the table must contain thead and tbody children
-			if (!$(this).find("thead tr th") || 
-			     !$(this).find("tbody tr td") )
-			{
-				alert("Error! $("+$(this).selector+") element is not a valid  HTML table");
-				return;
-			}
-		
-			//Currently the sortable selector must be unique
-			if ($(this).length > 1)
-			{
-				alert($(this).length);
-				alert("Error! $("+$(this).selector+") Table selector is not unique!");
-				return;
-			}
-		
-			//if not have id, we add one (ugly solution)
-			if (!$(this).attr("id"))
-			{
-				$(this).attr("id", (new Date()).getTime());
-			}
-			//Store the caller element's ID	
-			 callerID = $(this).attr("id");   
-			
-			$(this).find("thead tr th").each(function(){
-				var colIndex = $(this).index();
-				tableMatrix[colIndex] = [ ];
-							
-				//Table styling...
-				$(this).hover(function() {
-						$(this).css("cursor", "pointer");
-				});
 				
-				//click event on th element
-				$(this).on("click", function(event){
-					sortKey = $(event.target).index();
-					sortTableByKey(sortKey);		
-				});
-							
-			});
-										
-			//Put data from HTML table to tableMatrix variable
-			$(this).find("tbody tr td").each(function(){
+				activeTable = $(this);
+				console.log(activeTable);
+				
+				//Currently the table must contain thead and tbody children
+				if (!$(this).find("thead tr th") || 
+					 !$(this).find("tbody tr td") )
+				{
+					alert("Error! $("+$(this).selector+") element is not a valid  HTML table");
+					return;
+				}
+			
+				//Currently the sortable selector must be unique
+				if ($(this).length > 1)
+				{
+					alert($(this).length);
+					alert("Error! $("+$(this).selector+") Table selector is not unique!");
+					return;
+				}
+			
+				//if not have id, we add one (ugly solution)
+				if (!$(this).attr("id"))
+				{
+					$(this).attr("id", (new Date()).getTime());
+				}
+				//Store the caller elements ID	
+				 callerID = $(this).attr("id");   
+				
+				$(this).find("thead tr th").each(function(){
 					var colIndex = $(this).index();
-					var data = goodFormat($(this).html());
-					console.log(typeof data);
-					tableMatrix[colIndex].push(data);	
-			});
-		}
-});
+					tableMatrix[colIndex] = [ ];
+								
+					//Table styling...
+					$(this).hover(function() {
+						$(this).css("cursor", "pointer");
+					});
+					
+					//click event on th element
+					$(this).on("click", function(event){
+						sortKey = $(event.target).index();
+						sortTableByKey(sortKey);
+						
+								
+					});
+								
+				});
+											
+				//Put data from HTML table to tableMatrix variable
+				$(this).find("tbody tr td").each(function(){
+						var colIndex = $(this).index();
+						var data = goodFormat($(this).html());
+					//	console.log(typeof data);
+						tableMatrix[colIndex].push(data);	
+				});
+			}); //return for each
+		} //end of function
+} (jQuery));
